b6412c087a863e035bbe795a57df991fd4ba5b48
David Di Biase <1168397+davedbase@users.noreply.github.com>
2022-11-19 07:57:52 -0500

    Bumped deploy version

--- src/routes/status.ts
+++ src/routes/status.ts
@@ -5,7 +5,7 @@ import { success } from "../util/util";
  */
 export default async function votes() {
   return success({
-    version: "1.0.10",
+    version: "1.0.11",
     available: true,
     message: null,
   });
